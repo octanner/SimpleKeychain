244566efd608b135b47ba3fc3713358dc8283c18
Ben Norris <ben@bsn.io>
2016-02-11 11:36:18 -0700

    Initial blank framework

---
+++ SimpleKeychain/SimpleKeychain.h
@@ -0,0 +1,19 @@
+//
+//  SimpleKeychain.h
+//  SimpleKeychain
+//
+//  Created by Ben Norris on 2/11/16.
+//  Copyright © 2016 OC Tanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleKeychain.
+FOUNDATION_EXPORT double SimpleKeychainVersionNumber;
+
+//! Project version string for SimpleKeychain.
+FOUNDATION_EXPORT const unsigned char SimpleKeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleKeychain/PublicHeader.h>
+
+
